f1db61386648855f8c1c02d316a38a21c15c0f60
mirza-unas <unasmirza316@gmail.com>
2026-03-11 20:38:57 +0500

    setup backend server

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
