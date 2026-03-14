d76b00a7f6848db74b2fa46210b3859d41bf5d09
mirza-unas <unasmirza316@gmail.com>
2026-03-14 16:09:00 +0500

    setup auth screens ui design

---
+++ mobile/components/SafeScreen.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import COLORS from "../constants/colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+const SafeScreen = ({ children }) => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      {children}
+    </View>
+  );
+};
+
+export default SafeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+  },
+});
