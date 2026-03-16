0f99e7915b12663f6963b59f9f06855f506a6993
mirza-unas <unasmirza316@gmail.com>
2026-03-16 16:23:06 +0500

    Refactor mobile app layout and implement tab navigation
    - Enhanced RootLayout to manage authentication state and redirect users based on login status.
    - Removed the old index component and replaced it with a new tab navigation structure.
    - Added TabsLayout with screens for Home, Create, and Profile, each with appropriate icons.
    - Introduced new components for Create, Index, and Profile screens.

---
+++ mobile/app/(tabs)/_layout.jsx
@@ -0,0 +1,57 @@
+import { Tabs } from "expo-router";
+import COLORS from "../../constants/colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  const insets = useSafeAreaInsets();
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        headerTitleStyle: {
+          color: COLORS.primary,
+          fontWeight: "600",
+        },
+        headerShadowVisible: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.background,
+          borderTopWidth: 1,
+          borderTopColor: COLORS.border,
+          paddingTop: 5,
+          paddingBottom: insets.bottom,
+          height: 60 + insets.bottom,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
